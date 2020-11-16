29ce3a0c9346031dfd2c2ed06e8cd57e98c74bc1
miaoyu <miaoyu@sefonsoft.com>
2020-11-16 15:20:15 +0800

    modify test

--- src/react/__test__/createElement.test.tsx
+++ src/react/__test__/createElement.test.tsx
@@ -3,31 +3,31 @@ import { ReactElement } from "../createReactElement";
 
 describe("test createElement", () => {
 
-    it("render normal html", () => {
-      const ele = (<div title="123">test</div>) as any
-      expect(ele).toHaveProperty('getDom')
-      const dom =  ele.getDom("0")
-      expect(dom.outerHTML).toBe(`<div data-reactid="0" title="123">test</div>`)
-    })
+  it("render normal html", () => {
+    const ele = (<div title="123">test</div>) as any
+    expect(ele).toHaveProperty('getDom')
+    const dom = ele.getDom("0")
+    expect(dom.outerHTML).toBe(`<div data-reactid="0" title="123">test</div>`)
+  })
 
-  it("render function component", () => {
-    const Son = (props: { msg: string }) => {
-      return <h1>{props.msg}</h1>;
-    };
+  it("render element that has className", () => {
+    const ele = (<div className="test">test</div>) as any
+    expect(ele).toHaveProperty('getDom')
+    const dom = ele.getDom("0")
+    expect(dom.className).toBe("test")
+
+  })
+
+  it("render function component with props", () => {
     const App = (props: { msg: string }) => {
       return (
-        <div className="first">
-          <h3>{props.msg}</h3>
-          first
-          <span title="second">second</span>
-          <Son msg="this is son" />
-        </div>
+        <div>{props.msg}</div>
       );
     };
     const ele = (<App msg="this is msg" />) as any;
     const dom = ele.getDom("0");
     expect(dom.outerHTML).toBe(
-      `<div data-reactid="0" class="first"><h3 data-reactid="0-0">this is msg</h3>first<span data-reactid="0-2" title="second">second</span><h1 data-reactid="0-3">this is son</h1></div>`
+      `<div data-reactid="0">this is msg</div>`
     );
   });
 });
