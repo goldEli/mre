cb3ddd2562ba2a90108763c80ac8e0899d9d4988
miaoyu <miaoyu@sefonsoft.com>
2020-11-19 11:10:50 +0800

    modify config

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "./react";
 import {render} from "./react/dom"
 
-/** @jsx React.createElement */
 const ele = (
   <div title="123">
     <h1>Hello World</h1>
     <h2 >from React</h2>
   </div>
 )
+/** @jsx React.createElement */
 console.log(ele)
 const container = document.getElementById("root") as HTMLElement;
 render(ele, container);
